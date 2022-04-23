e5aa326a67974ff300fb072fed7e1b8726538fe3
kristinaprivoznikova <79633222+itskristy@users.noreply.github.com>
2022-04-23 13:55:22 +0200

    DB connection fixed

--- src/main/java/sk/uniza/kmikt/DatabaseConnectionManager.java
+++ src/main/java/sk/uniza/kmikt/DatabaseConnectionManager.java
@@ -1,16 +1,19 @@
 package sk.uniza.kmikt;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnectionManager {
 
     public static Connection getConnection() throws NamingException, SQLException {
-        DataSource ds;
+        /*DataSource ds;
         Context initContext;
         Connection con = null;
 
@@ -19,6 +22,17 @@ public class DatabaseConnectionManager {
         ds = (DataSource) env.lookup("mysqlpool");
 
         con = ds.getConnection();
+        return con;*/
+
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con= DriverManager.getConnection("jdbc:mysql://azuremysql-estrava.mysql.database.azure.com:3306/estrava","mysqladmin","estrava160.");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
         return con;
     }
 }
